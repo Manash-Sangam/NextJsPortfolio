ecbcdfb54a890f8e4e467682230b0f47a95bbdf2
Manash-Sangam <manashsangam04@gmail.com>
2025-01-30 17:05:57 +0530

    Fixed Skills UI

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -10,11 +10,11 @@ const Skill: React.FC<SkillProps> = ({ title, technologies }) => {
   return (
     <div className="skill p-4 bg-background rounded-lg mb-8">
       <h2 className="text-2xl font-bold mb-4 text-center">{title}</h2>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 sm:gap-x-16 w-full">
         {technologies.map((tech, index) => (
           <div key={index} className="flex flex-col items-center">
             <Image src={tech.imageUrl} alt={tech.name} width={64} height={64} className="rounded-lg skill-image" />
-            <span className="mt-2 text-sm">{tech.name}</span>
+            <span className="mt-2 text-xs">{tech.name}</span>
           </div>
         ))}
       </div>
